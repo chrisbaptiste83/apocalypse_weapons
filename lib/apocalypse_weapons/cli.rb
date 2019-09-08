fb014fc77cf53d639f6dbe50062f5623157cbce1
Chris Baptiste <chrisbaptiste83@gmail.com>
2019-09-08 09:09:36 -0700

    create methods in CLI class to display melee weapon and firearm info.

--- lib/apocalypse_weapons/cli.rb
+++ lib/apocalypse_weapons/cli.rb
@@ -27,7 +27,7 @@ def main_menu
      input = nil
        
         puts "Enter a weapon category for a list of the best weapons or type exit to exit." 
-        input=nil 
+        
         input = gets.strip.downcase 
          
         if input ==  "1" 
@@ -50,13 +50,90 @@ def main_menu
     end 
 
     def firearm_menu 
-        puts "Here is a list of the top firearms for surviving a zombie apocalypse:"
+
+        input = nil
+
+        puts "Here is a list of the top firearms for surviving a zombie apocalypse:"  
         ApocalypseWeapons::Firearm.list_firearms 
+        puts "Select a weapon for info and reasons why this weapon will help you survive the apocalypse."
+        
+        input = gets.strip.to_i 
+       
+         
+         selected_firearm = ApocalypseWeapons::Firearm.find_firearm(input) 
+
+         print_firearm_info(selected_firearm) 
+         puts "Would you like to view another weapon? "
+         
+
+            input = gets.strip.downcase
+            if input == "y"
+            main_menu
+            elsif input == "n"
+            puts ""
+            goodbye
+            else
+            puts ""
+            puts "I don't understand that answer."
+            main_menu 
+            end 
+
+    end 
+    
+    def print_firearm_info(firearm) 
+
+        puts ""
+        puts "----------- #{firearm.name}---------"
+        puts "" 
+        puts "---------------Description-----------"
+        puts "#{firearm.description}" 
     end 
 
+
+    def print_melee_weapon_info(melee_weapon)
+       
+        puts ""
+        puts "----------- #{melee_weapon.name}----"
+        puts "" 
+        puts "---------------Description----------"
+        puts "#{melee_weapon.description}" 
+        puts ""
+        puts "---------------History--------------"
+        puts "#{melee_weapon.history}" 
+
+    end 
+
+
+
+
     def melee_weapon_menu 
-        puts "here is a list of the best melee weapons for surviving a zombie apocalypse" 
+
+        input = nil
+
+        puts "Here is a list of the top firearmsmelee weapons for surviving a zombie apocalypse:"  
         ApocalypseWeapons::Melee_weapon.list_melee_weapons 
+        puts "Select a melee weapon for info and reasons why this weapon will help you survive the apocalypse."
+        
+        input = gets.strip.to_i 
+       
+         
+         selected_melee_weapon = ApocalypseWeapons::Melee_weapon.find_melee_weapon(input) 
+
+         print_melee_weapon_info(selected_melee_weapon) 
+         puts "Would you like to view another weapon? "
+         
+
+            input = gets.strip.downcase
+            if input == "y"
+            main_menu
+            elsif input == "n"
+            puts ""
+            goodbye
+            else
+            puts ""
+            puts "I don't understand that answer."
+            main_menu 
+            end 
     end 
 
 
